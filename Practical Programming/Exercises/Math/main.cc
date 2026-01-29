42382a4a50efcbbd7ee86817538055f67544af8d
StefanStringer <stefanstringer@gmail.com>
2026-01-29 16:04:44 +0100

    formatting

--- Practical Programming/Exercises/Math/main.cc
+++ Practical Programming/Exercises/Math/main.cc
@@ -1,18 +1,16 @@
-// main.cc ------------------------------------------------------------
+
 #include <iostream>
 #include <iomanip>
 #include "sfuns.h"
 
 int main()
 {
-    // ----- simple math constants -----
     std::cout << std::setprecision(15);
     std::cout << "sqrt(2) = " << std::sqrt(2.0) << '\n';
     std::cout << "21/5   = " << 21.0 / 5.0 << '\n';
     std::cout << "e^π    = " << std::exp(std::numbers::pi) << '\n';
     std::cout << "π·e    = " << std::numbers::pi * std::exp(1.0) << "\n\n";
 
-    // ----- Gamma and lnGamma for 1 .. 10 -----
     std::cout << "Gamma(x)  (Stirling approx)   |   lnGamma(x)\n";
     std::cout << "-----------------------------------------------\n";
 
