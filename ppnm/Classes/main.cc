ca612a5a0553fa20b458a2f03f30b7fc383cfadb
StefanStringer <stefanstringer@gmail.com>
2026-02-04 09:38:55 +0100

    make it work

--- ppnm/Classes/main.cc
+++ ppnm/Classes/main.cc
@@ -7,15 +7,21 @@ int main(){
    // hello();
     double a = 1.0;
     double b = a;
-    if(a==b) {std::cout << ("a==b\n") << std::endl;}
-    else {std::cout << "a!=b\n" << std::endl;}
+
+    if(a==b) {
+        std::cout << "a==b \n" << std::endl;
+    }
+    else {
+        std::cout << "a!=b\n" << std::endl;
+    }
     std::vector<double> v {1,2,3};
-    for(size_t i=0;i<v.size();i++)std::printf("%g ",v[i]);
+    for(size_t i=0;i<v.size();i++) {
     std::cout << ("\n") << std::endl;
-    for(auto vi : v) std::cout << ("%g ",vi) << std::endl; //can say auto or double, it is the same
-    std::cout << ("\n") << std::endl;
-    for(auto vi : v) vi=6;
-    for(auto vi : v) std::cout << ("%g ",vi) << std::endl;
+    }
+    //for(auto vi : v) std::cout << ("%g ",vi) << std::endl; //can say auto or double, it is the same
+    //std::cout << ("\n") << std::endl;
+    //for(auto vi : v) vi=6;
+    //for(auto vi : v) std::cout << ("%g ",vi) << std::endl;
     // auto u = v;
     // auto& w=v; // placing & is a reference
 return 0;
