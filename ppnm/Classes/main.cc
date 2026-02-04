956aa6ffd3c50bc2255d3dc4f512b5de42a7f2af
StefanStringer <stefanstringer@gmail.com>
2026-02-04 09:36:18 +0100

    print statements better

--- ppnm/Classes/main.cc
+++ ppnm/Classes/main.cc
@@ -1,20 +1,22 @@
 #include<iostream>
 #include<vector>
 #include<cstdio>
-#include"hello.h"
+//#include<hello.h>
 
 int main(){
-    hello();
+   // hello();
     double a = 1.0;
     double b = a;
-    if(a==b) {std::printf"a==b\n";}
-    else {std::printf"a!=b\n";}
+    if(a==b) {std::cout << ("a==b\n") << std::endl;}
+    else {std::cout << "a!=b\n" << std::endl;}
     std::vector<double> v {1,2,3};
     for(size_t i=0;i<v.size();i++)std::printf("%g ",v[i]);
-    std::printf("\n");
-    for(auto vi : v) std::printf("%g ",vi); //can say auto or double, it is the same
-    std::printf("\n");
-    auto u = v;
-    auto& w=v; // placing & is a reference
+    std::cout << ("\n") << std::endl;
+    for(auto vi : v) std::cout << ("%g ",vi) << std::endl; //can say auto or double, it is the same
+    std::cout << ("\n") << std::endl;
+    for(auto vi : v) vi=6;
+    for(auto vi : v) std::cout << ("%g ",vi) << std::endl;
+    // auto u = v;
+    // auto& w=v; // placing & is a reference
 return 0;
 }
