96a870418ecc7e869bdf6c8e92cf8f53407a6077
StefanStringer <stefanstringer@gmail.com>
2026-02-04 09:30:30 +0100

    update

--- ppnm/Classes/main.cc
+++ ppnm/Classes/main.cc
@@ -12,6 +12,8 @@ int main(){
     std::vector<double> v {1,2,3};
     for(size_t i=0;i<v.size();i++)std::printf("%g ",v[i]);
     std::printf("\n");
+    for(auto vi : v) std::printf("%g ",vi); //can say auto or double, it is the same
+    std::printf("\n");
     auto u = v;
     auto& w=v; // placing & is a reference
 return 0;
