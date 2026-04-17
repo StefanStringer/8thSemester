ce0b6110101fc9b18a0a3d6f7ac92e5dcf0ede47
StefanStringer <stefanstringer@gmail.com>
2026-04-17 13:45:26 +0200

    Questrion A done

--- ppnm/Homework/MonteCarlo/main.cc
+++ ppnm/Homework/MonteCarlo/main.cc
@@ -3,6 +3,7 @@
 #include <cmath>
 #include <random>
 #include <functional>
+#include <iomanip>
 #include "matrix.hpp"
 
 using namespace std;
@@ -16,12 +17,13 @@ pair<double,double> plainmc(
     int N){
     int dim = a.size();
     double V = 1;
-    for(int i=0;i<dim;i++) V *= (b[i]-a[i]);
+    for(int i=0;i<dim;i++)
+        V *= (b[i] - a[i]);
 
     //this is how er generate randome values
     random_device rd;
     mt19937 gen(rd());
-    uniform_real_distribution<> dist(0.0,1.0);
+    uniform_real_distribution<> dist(0.0, 1.0);
 
     double sum=0, sum2=0;
     lineq::vector x(dim);
@@ -36,44 +38,62 @@ pair<double,double> plainmc(
     }
 
     double mean = sum/N;
-    double sigma = sqrt(sum2/N - mean*mean);
+    double variance = (sum2/N - mean*mean);
+    double sigma = (variance > 0) ? sqrt(variance) : 0.0;
 
     return {mean*V, sigma*V/sqrt(N)};
 }
 
-int main(){
-    ofstream data("data.txt");
-    std::cout << "---------- Part A ----------\n\n";
-    // --- Test: area of unit circle ---
-    auto f_circle = [](lineq::vector x){
-        if(x[0]*x[0] + x[1]*x[1] <= 1.0) return 1.0;
-        else return 0.0;
+void QuestionA() {
+    cout << "========== Question A: Unit Circle Area ==========\n\n";
+    
+    // Function: Indicator for unit circle (x^2 + y^2 <= 1)
+    auto f_circle = [](lineq::vector x) -> double {
+        if (x[0] * x[0] + x[1] * x[1] <= 1.0) 
+            return 1.0;
+        else 
+            return 0.0;
     };
 
-    lineq::vector a = {-1,-1};
-    lineq::vector b = {1,1};
-
+    // Integration bounds: [-1, 1] x [-1, 1]
+    lineq::vector a = {-1.0, -1.0};
+    lineq::vector b = {1.0, 1.0};
+    
     double exact = M_PI;
 
-    cout << "Monte Carlo integration (unit circle area)\n";
-    cout << "Exact value = " << exact << "\n\n";
+    cout << "Target: Area of unit circle\n";
+    cout << "Exact value (PI) = " << fixed << setprecision(8) << exact << "\n\n";
+    
+    cout << " N          Result        Error    Actual Error\n";
+    cout << "------------------------------------------------------\n";
 
-    cout << " N        result        est_error     actual_error\n";
+    // Open file for plotting data (separate per question)
+    ofstream data_file("data_A.txt");
+    data_file << "# N Estimated_Error Actual_Error\n";
 
-    for(int N = 1000; N <= 1000000; N *= 2){
-        auto [res, err] = plainmc(f_circle, a, b, N);
-        double actual_error = fabs(res - exact);
+    // Run for N = 1000, 2000, 4000 ... 1,000,000
+    for (int N = 1000; N <= 1000000; N *= 2) {
+        auto [result, est_error] = plainmc(f_circle, a, b, N);
+        double actual_error = fabs(result - exact);
 
-        cout << N << "  "
-             << res << "  "
-             << err << "  "
-             << actual_error << "\n";
+        cout << setw(6) << N << "  "
+             << setw(12) << fixed << setprecision(6) << result << "  "
+             << setw(12) << est_error << "  "
+             << setw(12) << actual_error << "\n";
 
-        data << N << " " << err << " " << actual_error << "\n";
+        // Write to data file for gnuplot
+        data_file << N << " " << est_error << " " << actual_error << "\n";
     }
 
-    cout << "\nData written to data.txt\n";
-    cout << "Plot will be generated as error.svg\n";
+    data_file.close();
+    
+    cout << "\n------------------------------------------------------\n";
+    cout << "Data saved to 'data_A.txt'\n";
+}
+
+
+int main(){
+    QuestionA();
 
     return 0;
 }
